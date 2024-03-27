a3b5f80205421e1c487446ea4af220bc33399dd9
Rumcajs121 <z.miara45@gmail.com>
2024-03-27 12:30:35 +0100

    I refactoring and prepare new version method in result response in dapper. I readed new SqlDataAcces when i use connection string and two method Load and Save. I rextract interface witch use for Data access when read implemented CRUD method.

---
+++ MinimalWebApiLearn/Endpoints/ISqlDataAcces.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MinimalWebApiLearn.Endpoints
+{
+    public interface ISqlDataAcces
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connetionID = "MinimalWebApi");
+        Task SaveData<T>(string storedProcedure, T parameters, string connetionID = "MinimalWebApi");
+    }
+}
---
+++ MinimalWebApiLearn/Endpoints/ToDoListData.cs
@@ -0,0 +1,21 @@
+﻿using MinimalWebApiLearn.Models;
+
+namespace MinimalWebApiLearn.Endpoints
+{
+    public class ToDoListData
+    {
+        private readonly ISqlDataAcces _db;
+
+        public ToDoListData(ISqlDataAcces db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<Assignment>> GetAllToDoListS() => _db.LoadData<Assignment, dynamic>("Xyz", new { });
+        public async Task <Assignment?> GetToDoList(int id)
+        {
+            var results=await _db.LoadData<Assignment,dynamic>("Xyz", new {Id=id});
+            return results.FirstOrDefault();
+        }
+    }
+}

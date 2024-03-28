8c5b55d2511897719e104cfe943dccb2cd85f28c
Rumcajs121 <z.miara45@gmail.com>
2024-03-28 17:57:30 +0100

    I added rest more method #3

--- MinimalWebApiLearn/Endpoints/ToDoListData.cs
+++ MinimalWebApiLearn/Endpoints/ToDoListData.cs
@@ -14,8 +14,11 @@ namespace MinimalWebApiLearn.Endpoints
         public Task<IEnumerable<Assignment>> GetAllToDoListS() => _db.LoadData<Assignment, dynamic>("dbo.spToDoListsDb_GettAll", new { });
         public async Task <Assignment?> GetToDoList(int id)
         {
-            var results=await _db.LoadData<Assignment,dynamic>("Xyz", new {Id=id});
+            var results=await _db.LoadData<Assignment,dynamic>("dbo.spToDoListsDb_GettOneTask", new {Id=id});
             return results.FirstOrDefault();
         }
+        public Task InsertToDoList(Assignment assignment) =>_db.SaveData("dbo.spToDoListsDb_CreateNewTask", new{ assignment.Description, assignment.EndDate});
+        public Task UpdateTask(Assignment assignment) =>_db.SaveData("dbo.spToDoListsDb_EditTask", new{ assignment.Description, assignment.EndDate});
+        public Task DeleteTask(int id) =>_db.SaveData("dbo.spToDoListsDb_DeleteTaskId",new{ Id=id});
     }
 }

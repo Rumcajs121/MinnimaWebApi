b7b6d0b7902fa4445ba9d5eb8751a5150c1d1d1c
Rumcajs121 <z.miara45@gmail.com>
2024-04-09 20:18:11 +0200

    ops(MistakeCommit:3ef39eb):I forgot one ToDoValidator file add for my last commit. refers to: #6

---
+++ MinimalWebApiLearn/Models/ToDoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MinimalWebApiLearn.Models;
+
+namespace MinimalWebApiLearn;
+
+public class ToDoValidator:AbstractValidator<AssignmentDto>
+{
+    public bool BeAValidDate(string value){
+        return DateTime.TryParseExact(value, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out _);
+    }
+    public ToDoValidator()
+    {
+        RuleFor(x=>x.Description).NotEmpty()
+                                .MinimumLength(3)
+                                .WithMessage("Description is empty You read Task");
+        RuleFor(x=>x.EndDate)
+        .Must(date=>BeAValidDate(date.ToString("yyyy-MM-dd")))
+        .WithMessage("Data is wrong format");
+    }
+}
